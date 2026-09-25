4d7b3c3298e4087be8ff525490237adf7762e47c
Quan Pham <mqfiddlestick@gmail.com>
2026-09-24 20:15:38 -0400

    swap interact button position

--- src/init.go
+++ src/init.go
@@ -281,14 +281,14 @@ func init() {
 	interactPadY := 180 / 2
 	mobileButtons = []TouchButton {
 		{ // A button
-			ButtonType: ButtonInteractA,
+			ButtonType: ButtonInteractD,
 			boundX: interactPadX,
 			boundY: interactPadY,
 			boundWidth: buttonSize,
 			boundHeight: buttonSize,
 		},
 		{ // D button
-			ButtonType: ButtonInteractD,
+			ButtonType: ButtonInteractA,
 			boundX: interactPadX - 2 * buttonSize,
 			boundY: interactPadY,
 			boundWidth: buttonSize,
